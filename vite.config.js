780dba229f3a742f2d89abd763936976f1126881
Kamasah Dickson <kamasahdickson19@gmail.com>
2022-12-29 13:32:18 +0000

    final production

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/React-redesigned-enigma/",
+	base: "/base/React-redesigned-enigma",
 	plugins: [react()],
 });
